3e4a225932a44a8eec380836e47b0966956fccb0
Jorge Portatil <geferon38@gmail.com>
2019-11-12 14:52:38 +0100

    Update

---
+++ OpenGameMonitorWorker/Services/FTPService/FTPMembershipProvider.cs
@@ -0,0 +1,25 @@
+﻿using FubarDev.FtpServer.AccountManagement;
+using Microsoft.AspNetCore.Identity;
+using OpenGameMonitorLibraries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenGameMonitorWorker
+{
+    class FTPMembershipProvider : IMembershipProvider
+    {
+        UserManager<MonitorUser> _userManager;
+        public FTPMembershipProvider(UserManager<MonitorUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<MemberValidationResult> ValidateUserAsync(string username, string password)
+        {
+
+            return new MemberValidationResult(MemberValidationStatus.InvalidLogin);
+        }
+    }
+}
---
+++ OpenGameMonitorWorker/Services/FTPService/HostedFTPService.cs
@@ -0,0 +1,30 @@
+﻿using FubarDev.FtpServer;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenGameMonitorWorker
+{
+    class HostedFTPService : IHostedService
+    {
+        IFtpServerHost _ftpServerHost;
+
+        public HostedFTPService(IFtpServerHost ftpServerHost)
+        {
+            _ftpServerHost = ftpServerHost;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _ftpServerHost.StartAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _ftpServerHost.StopAsync();
+        }
+    }
+}
